947755f073488776425eb84d30b8cb2f66db6111
Hilal <140879548+BinaryByteDev1@users.noreply.github.com>
2024-02-07 02:26:20 +0530

    Add files via upload

---
+++ Math/Math_Functions.php
@@ -0,0 +1,65 @@
+<?php
+
+// pi() function returns the value of PI
+echo "Value of PI = ", pi();
+
+// min() returns the minimum value 
+// max() returns the maximum value
+echo"<br>";
+echo "Minumum = ", min(10, 3 , 50 , 55 , 60);
+echo"<br>";
+echo  "Maximum = ", max(100, 30 ,55 , 88 ,7);
+
+echo"<br>";
+// abs() function returns the absolute
+
+echo "Absolute value = ", abs(-5.7) , "<br>";
+echo "Absolute value = ", abs(-0.1) , "<br>";
+echo "Absolute value = ", abs(-2.7) , "<br>";
+echo "Absolute value = ", abs(-10.11) , "<br>";
+
+
+
+//sqrt() function returns the square root of a number
+echo "square root = ", sqrt(84),"<br>";
+echo "square root = ", sqrt(16),"<br>";
+echo "square root = ", sqrt(64),"<br>";
+echo "square root = ", sqrt(36),"<br>";
+
+
+echo"<br>";
+
+
+// round() Math Function
+
+
+
+echo round(0.80),"<br>";
+echo round(2.6),"<br>";
+echo round(1.4),"<br>";
+echo round(6.3),"<br>";
+
+
+
+echo"<br>";
+
+//  rand() functio generates the rendom numbers 
+
+echo rand(), "<br>";
+echo rand(1, 10), "<br>";
+echo rand(1 , 100), "<br>";
+echo rand(100 , 200), "<br>";
+echo rand(100 , 1000), "<br>";
+
+
+echo"<br>";
+
+
+
+
+
+
+
+
+
+?>
