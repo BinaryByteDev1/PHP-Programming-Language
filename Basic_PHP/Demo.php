4b8a373776be70332957630c90194d7bd9b3ca52
Hilal <140879548+BinaryByteDev1@users.noreply.github.com>
2024-02-06 20:33:41 +0530

    Add files via upload

---
+++ Basic_PHP/Demo.php
@@ -0,0 +1,10 @@
+<?php 
+
+echo "My first PHP script!", "<br>";
+echo "My first PHP script!", "<br>";
+echo "My first PHP script!" ,"<br>";
+echo "My first PHP script!", "<br>";
+
+print "Hello Php Programming Language...";
+
+ ?>
