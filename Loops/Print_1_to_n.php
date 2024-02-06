acf35545509cdd06590a1ba1ad104d81982ec40f
Hilal <140879548+BinaryByteDev1@users.noreply.github.com>
2024-02-06 20:34:40 +0530

    Add files via upload

---
+++ Loops/Print_1_to_n.php
@@ -0,0 +1,30 @@
+<?php
+
+// using for loop
+$n = 200;
+for($i = 1; $i <= $n; $i++){
+    
+    echo $i , "    ";
+}
+
+
+echo"<br> <br>";
+
+
+// using while loop
+$i = 1;
+$m = 200;
+while ($i != $m){
+   echo $i,"    ";
+   $i++;
+}
+
+echo"<br> <br>";
+// using do while loop
+
+do{
+    echo $x,"    ";
+    $x++;
+}while($x != 200);
+
+?>
