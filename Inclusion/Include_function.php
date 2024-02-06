866cc3c7e301c265d68ecd4197d495052e5758e4
Hilal <140879548+BinaryByteDev1@users.noreply.github.com>
2024-02-06 23:07:07 +0530

    Add files via upload

---
+++ Inclusion/Include_function.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+   <meta charset="UTF-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1">
+   <title></title>
+</head>
+
+<body>
+
+<h1>This is Include Function </h1>
+<?php
+include "Data_file.php";
+?>
+
+</body>
+
+</html>
---
+++ Inclusion/required_function.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+   <meta charset="UTF-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1">
+   <title></title>
+</head>
+
+<body>
+
+<h1>This is Require Function </h1>
+<?php
+require "Data_file.php";
+?>
+
+</body>
+
+</html>
